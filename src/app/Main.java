3de48ea3352ef0ae3f13d200d504abe502ae5c45
Ian Stettner <student@ian-stettner.dalton.org>
2018-01-29 11:33:18 -0500

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println ("Ravioli Ravioli what it inside doth Pocketoli");
+    }
+}
